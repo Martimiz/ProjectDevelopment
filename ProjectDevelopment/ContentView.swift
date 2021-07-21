67c93711944a95806891b144186db68d94bd48d7
Martimiz <martine@balbus.nl>
2021-07-21 13:51:33 +0200

    Initial Commit

---
+++ ProjectDevelopment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectDevelopment
+//
+//  Created by Martine Bloem on 21/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectDevelopment/ProjectDevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDevelopmentApp.swift
+//  ProjectDevelopment
+//
+//  Created by Martine Bloem on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
