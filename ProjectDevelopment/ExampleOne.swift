20e7be37fc283538bf11c321024893d190871643
Martimiz <martine@balbus.nl>
2021-07-21 15:43:11 +0200

    ExampleOne added

---
+++ ProjectDevelopment/ExampleOne.swift
@@ -0,0 +1,26 @@
+//
+//  ExampleOne.swift
+//  ProjectDevelopment
+//
+//  Created by Martine Bloem on 21/07/2021.
+//
+
+import Foundation
+
+struct ExampleOne {
+    
+    // MARK: - properties
+    
+    var someProperty: Int = 1
+    
+    // MARK: - functions
+    
+    /// This function prints that it is doing something
+    func doSomething() {
+        // FIXME: - Make it so it actually does something
+        print("doing somthing")
+    }
+    
+    
+    
+}
